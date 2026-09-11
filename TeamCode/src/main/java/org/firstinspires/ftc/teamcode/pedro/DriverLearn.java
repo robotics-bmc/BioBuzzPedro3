85ea2e1c85769f10104485e66df0baa3055f9940
leerbrobotics <leerb@bishopmoore.org>
2026-09-11 14:41:49 -0400

    first commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedro/DriverLearn.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode.pedro;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="Basic: Linear OpMode", group="Linear OpMode")
+public class DriverLearn {
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedro/Tuning.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedro/Tuning.java
@@ -1,5 +1,15 @@
 package org.firstinspires.ftc.teamcode.pedro;
 
+import com.pedropathing.revhub.drivetrains.Mecanum;
+import com.pedropathing.tuning.autotune.Procedure;
+import com.pedropathing.tuning.autotune.Tuner;
+import org.firstinspires.ftc.teamcode.pedro.procedures.MecanumTuner;
+
+import org.firstinspires.ftc.teamcode.pedro.procedures.Tests;
+
 public class Tuning {
     // Tuners go here
+
 }
+
+
