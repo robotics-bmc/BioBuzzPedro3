1577c7817dc2a68485bd7efadb6b39bf00de4e1c
Teacher <robotics@bishopmoore.org>
2026-09-22 16:32:40 -0400

    tuning procedure

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedro/Tuning.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedro/Tuning.java
@@ -5,10 +5,19 @@ import com.pedropathing.tuning.autotune.Procedure;
 import com.pedropathing.tuning.autotune.Tuner;
 import org.firstinspires.ftc.teamcode.pedro.procedures.MecanumTuner;
 
+import org.firstinspires.ftc.teamcode.pedro.procedures.PinpointTuner;
 import org.firstinspires.ftc.teamcode.pedro.procedures.Tests;
 
 public class Tuning {
     // Tuners go here
+    @Tuner
+    public static Procedure mecanumTuner() {
+        return new MecanumTuner();
+    }
+    @Tuner
+    public static Procedure pinpointTuner() {
+        return new PinpointTuner();
+    }
 
 }
 
